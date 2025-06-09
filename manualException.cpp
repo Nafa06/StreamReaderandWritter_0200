6634d5627f1616365f030d572e830261b4aaf565
Nafa06 <naufalanggardi09@gmail.com>
2025-06-09 15:21:05 +0700

    Merapikan code

--- manualException.cpp
+++ manualException.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     try
     {
         cout << "Selamat belajar di prodi TI UMY" << endl;
         throw 3.5; // melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan dieksekusi" << endl;
     }
-    catch(int a)
+    catch (int a)
     {
         // blok ini akan dieksekusi
         cout << a << ": Pengecualian akan dieksekusi" << endl;
     }
 
-    catch(...){
+    catch (...)
+    {
         // jika selain integer maka blok ini akan dieksekusi
         cout << "Default pengecualian dieksekusi" << endl;
     }
