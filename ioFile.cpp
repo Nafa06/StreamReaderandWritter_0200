416c3eda31f1bd5d66e8ad940451ab4712531dd6
Nafa06 <naufalanggardi09@gmail.com>
2025-06-09 14:42:54 +0700

    membuka file dan menampilkan isi file, kemudian menutup file

--- ioFile.cpp
+++ ioFile.cpp
@@ -29,5 +29,22 @@ int main(){
 
     // membuat objek input file stream
     ifstream infile;
+    // membuka file yang dituliskan
+    infile.open("contohfile.txt");
+
+    cout << endl << "> Membuka dan membaca file " << endl;
+    // jika file ada, maka:
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            // dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        // menutup file
+        infile.close();
+        
+    }
     
 }
