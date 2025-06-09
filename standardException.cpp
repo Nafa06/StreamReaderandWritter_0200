556c8bd6404b1d703715a3e4b045c034654fedec
Nafa06 <naufalanggardi09@gmail.com>
2025-06-09 15:26:19 +0700

    membuat file baru manualexception

---
+++ standardException.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <exception>
+// untuk objek exception yang akan digunakan
+#include <array>
+// untuk objek array yang akan kita gunakan
+using namespace std;
+
+int main()
+{
+    cout << "Awal program" << endl; // penanda 1:...
+    try
+    {
+        array<int, 3> data = {10, 20, 30};
+        // pesan array integer 3 elemen
+        cout << data.at(5) << endl;
+        // memanggil array elemen ke 5
+    }
+    catch (exception& e)
+    {
+        // penangkap menggunakan objek exception
+        
+    }
+
+    catch (...)
+    {
+        // jika selain integer maka blok ini akan dieksekusi
+        cout << "Default pengecualian dieksekusi" << endl;
+    }
+    return 0;
+}
