4657f975b3f9c2a8ae6c2f537162749911fb17d9
Nafa06 <naufalanggardi09@gmail.com>
2025-06-09 14:25:34 +0700

    membuat kelas ofstream dan objek outfile

--- ioFile.cpp
+++ ioFile.cpp
@@ -4,5 +4,12 @@
 using namespace std;
 
 int main(){
-    
+    string baris;
+
+    // membuat objek output file stream
+    ofstream outfile;
+    // membuka file untuk ditulisi
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 }
